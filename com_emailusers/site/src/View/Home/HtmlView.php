313620e86285bea649cae5254d5a056c8bdf2ecf
YanaMall <75187993+YanaMall@users.noreply.github.com>
2022-07-06 08:14:33 -0400

    Add files via upload

---
+++ com_emailusers/site/src/View/Home/HtmlView.php
@@ -0,0 +1,23 @@
+<?php
+namespace AM\Component\emailusers\Site\View\Home;
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_emailusers
+ *
+ * @copyright   Copyright (C) 2020 John Smith. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * View for the user identity validation form
+ */
+class HtmlView extends BaseHtmlView {    
+    public function display($template = null) 
+    {
+        parent::display($template);
+    }
+}
