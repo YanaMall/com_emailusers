89f8b2983c679655036e592bc9d82fed636e0bd8
YanaMall <75187993+YanaMall@users.noreply.github.com>
2022-07-07 06:01:10 -0400

    Update default.php

--- site/tmpl/home/default.php
+++ site/tmpl/home/default.php
@@ -13,5 +13,61 @@
 
      // No direct access to this file
      defined('_JEXEC') or die( 'Restricted access' );
+
+     $sendDate = date_create()->modify('Saturday this week')->format('Y-m-d');
+     $group_id = 11;
+     $access = new JAccess();
+     $allStudentIDs = $access->getUsersByGroup($group_id);
+
+     $reportStudentIDs = [];
+     foreach ($weekReports as $report)
+     {
+          array_push($reportStudentIDs, $report[1]);
+     }
+     $noReportStudentIDs = array_diff($allStudentIDs, $reportStudentIDs);
+
+     $rows = '';
+     foreach ($noReportStudentIDs as $id)
+     {
+          $user = JFactory::getUser($id);
+          $rows .= '<tr>';
+          $rows .= '<td>' . $user->name . '</td>';
+          $rows .= '<td>' . $user->get('email') . '</td>';
+          $rows .= '</tr>';
+     }
+
+     if(isset($_POST['button_pressed']))
+     {
+          $content = 'This email is to remind you to send in you weekly report.';
+          foreach ($noReportStudentIDs as $id) 
+          {
+               $user = JFactory::getUser($id);
+               $to = $user->get('email');
+               mail($to, 'Programming Team Report', $content);
+          }
+
+          echo 'Email Sent.';
+     }
 ?>
-<h2>Hello world!</h2>
+
+<style>
+    .section {
+        margin-bottom: 20px;
+        padding: 10px;
+    }
+</style>
+
+<div class="displayStudents">
+     <h2>Students who have not completed this week's report.</h2>
+</div>
+<table class="table">
+     <tr>
+     <th>Name</th>
+     <th>Email</th>
+     </tr>
+     <?php echo $rows; ?>
+</table>
+<form action="" method="post">
+    <input type="submit" value="Send out emails about reports" />
+    <input type="hidden" name="button_pressed" value="1" />
+</form>
