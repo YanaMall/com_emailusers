ef9600dae6e87ad683868766a23a3d5bcbd84a25
YanaMall <75187993+YanaMall@users.noreply.github.com>
2022-07-07 05:06:23 -0400

    Add files via upload

---
+++ site/src/Controller/DisplayController.php
@@ -0,0 +1,40 @@
+<?php
+namespace AM\Component\emailusers\Site\Controller;
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Factory;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_emailusers
+ *
+ * @copyright   Copyright (C) 2020 John Smith. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * emailusers Component Controller
+ * @since  0.0.2
+ */
+
+
+class DisplayController extends BaseController {
+    public function display($cachable = false, $urlparams = array()) {        
+        $document = Factory::getDocument();
+        //$document->setTitle('Email Notifications');
+        //$app = Factory::getApplication();
+        //$menu = $app->getMenu();
+        //$items = $menu->getMenu();
+        //foreach ($items as $item) {
+        //    if($item->title == 'Email Notifications') {
+        //        $menu->setActive($item->id);
+        //    }
+        //}
+        $viewName = $this->input->getCmd('view', 'login');
+        $viewFormat = $document->getType();
+        $view = $this->getView($viewName, $viewFormat);
+        $view->document = $document;
+        $view->display();
+    }  
+}
---
+++ site/tmpl/home/default.php
@@ -0,0 +1,17 @@
+<?php 
+
+     use Joomla\CMS\Factory;
+     use Joomla\CMS\Language\Text;
+
+     /**
+      * @package     Joomla.Administrator
+      * @subpackage  com_emailusers
+      *
+      * @copyright   Copyright (C) 2022 Ayiana Mallory. All rights reserved.
+      * @license     GNU General Public License version 3; see LICENSE
+      */
+
+     // No direct access to this file
+     defined('_JEXEC') or die( 'Restricted access' );
+?>
+<h2>Hello world!</h2>
